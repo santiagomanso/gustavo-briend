648e2baf8f0b5d87ec8d0a787887cf23a10fbd42
santiagomanso <santiagomanso@web.de>
2022-09-13 21:23:06 +0200

    🚀 testing whatsapp button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Navbar from './components/Navbar'
+import FloatingWhatsApp from 'react-floating-whatsapp'
+import gustavoProfile from './assets/whatsappProfile.png'
 
 function App() {
   return (
     <div className='app h-screen'>
       <Navbar />
+      <FloatingWhatsApp
+        phoneNumber='+5493794508606'
+        accountName='Gustavo Briend'
+        statusMessage='Generalmente responde en 1hr'
+        chatMessage='Hola soy Gustavo, como puedo ayudarte?'
+        allowClickAway
+        placeholder='Escriba su consulta'
+        allowEsc
+        avatar={gustavoProfile}
+      />
     </div>
   )
 }
