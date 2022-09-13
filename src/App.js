e213e7d7e3400c6579d82ebac4c97ee1be049f77
santiagomanso <santiagomanso@web.de>
2022-09-13 22:01:17 +0200

    🚑 overflow hidden testing

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import gustavoProfile from './assets/whatsappProfile.png'
 
 function App() {
   return (
-    <div className='app h-screen'>
+    <div className='app h-screen overflow-hidden'>
       <Navbar />
       <FloatingWhatsApp
         phoneNumber='+5493794508606'
         accountName='Gustavo Briend'
         statusMessage='Generalmente responde en 1hr'
-        chatMessage='Hola soy Gustavo, como puedo ayudarte?'
+        chatMessage='Hola soy Gustavo, en que puedo ayudarte?'
         allowClickAway
         placeholder='Escriba su consulta'
         allowEsc
